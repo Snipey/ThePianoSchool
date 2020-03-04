25e32c04c1c898bc609776bc76e0410eab1cd712
Stephen F <snipeydev@gmail.com>
2020-03-04 15:28:02 -0800

    Updated picture and bio

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import Layout from "../components/layout";
-import Helmet from 'react-helmet'
+import Helmet from 'react-helmet';
+let schedule = "https://calendar.google.com/calendar/selfsched?sstoken=UUVmdjgzY21fdmxsfGRlZmF1bHR8MTQ0NTIyZWRjMWExOTcwNGFhOTc4ZDhkYTcxN2UxZWM"
+let contact = "https://forms.gle/vo11ir5vfmN35NhD7"
 const Home = () => (
   <Layout>
     <Helmet
@@ -8,20 +10,25 @@ const Home = () => (
     />
     <div className="flex flex-col items-center justify-center h-screen">
       <div className="mb-6 flex flex-col items-center">
-        <img className="rounded-full border-2 border-purple-600 hover:border-red-500 duration-500 transition" src="https://picsum.photos/200/200"></img>
-        <h1 className="pt-2">The Piano School</h1>
+        <img className="rounded-full border-2 border-purple-600 hover:border-red-500 duration-500 transition w-64 h-64" src={require('../public/cover.jpg')}></img>
+        <h1 className="pt-2 mb-2">The Piano School</h1>
+        <span className="break-normal mx-12 text-center">
+          Hello, my name is Stephanie and I teach Piano. Click schedule to see when you can <span className="underline text-white font-bold hover:text-purple-500"><a href={schedule}>schedule</a></span> a lesson. <br/>
+           <b className="text-red-500 underline">Have not booked with me before?</b> <br/>
+            Click the contact link and fill out the form for more information on what I offer.
+        </span>
       </div>
 
-      <ul className="flex-col w-1/4">
+      <ul className="flex-col md:w-1/4 w-3/4">
         <li className="flex-1 mt-4">
           <a className="text-center block border border-purple-500 rounded py-2 px-4 bg-purple-500 hover:bg-purple-700 text-white" 
-              href="https://calendar.google.com/calendar/selfsched?sstoken=UUVmdjgzY21fdmxsfGRlZmF1bHR8MTQ0NTIyZWRjMWExOTcwNGFhOTc4ZDhkYTcxN2UxZWM">
+              href={schedule}>
                 Schedule
           </a>
         </li>
         <li className="flex-1 mt-4">
           <a className="text-center block border border-purple-500 rounded py-2 px-4 bg-purple-500 hover:bg-purple-700 text-white" 
-              href="https://forms.gle/vo11ir5vfmN35NhD7">
+              href={contact}>
                 Contact
           </a>
         </li>
